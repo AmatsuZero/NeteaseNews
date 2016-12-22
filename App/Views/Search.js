f1ab4c0ec4018897ab7af8a8581503cc8bd6b881
姜振华 <jiangzhenhua@123juzi.com>
2016-12-22 20:33:31 +0800

    1. 搜索页第一页完成，列表页待完成。要注意的是近期热点的View部分；最开始参照原生工程设置了每个label的宽高，导致了宽高的压缩，此时设置flex均无效，宽高和flexbox的属性似乎是有冲突的 2. 放弃了原生Button，以后还是自己做Button吧……

--- App/Views/Search.js
+++ App/Views/Search.js
@@ -8,15 +8,16 @@ import {
     View,
     Image,
     InteractionManager,
+    TouchableWithoutFeedback,
     TouchableOpacity,
     Dimensions,
     Navigator,
-    Button,
     TextInput,
     Platform,
     DeviceEventEmitter
 } from "react-native";
 import {Navibarheight} from "../Model/Constants";
+import LoadingView from "../Component/LoadingView";
 
 export default class Search extends React.Component {
 
@@ -24,6 +25,8 @@ export default class Search extends React.Component {
         super(props);
         this.state = {
             hotwords: null,
+            searchTxt: '',
+            isResultList: false
         }
     }
 
@@ -36,7 +39,7 @@ export default class Search extends React.Component {
     }
 
     searchForNews(key) {
-        console.log(event.nativeEvent.text);
+        console.log(key);
     }
 
     //返回
@@ -54,6 +57,7 @@ export default class Search extends React.Component {
                 <TextInput
                     autoCapitalize="none"
                     style={styles.inputStyle}
+                    onChangeText={(text)=>{this.setState({searchTxt:text})}}
                     onSubmitEditing={(event)=>this.searchForNews(event.nativeEvent.text)}
                     returnKeyType='search'
                     placeholder='搜索'
@@ -61,21 +65,90 @@ export default class Search extends React.Component {
                     clearButtonMode="while-editing"
                     enablesReturnKeyAutomatically={true}
                 />
-                <Button
-                    title="取消"
-                    onPress={()=>this.goBack()}
-                />
+                <TouchableWithoutFeedback onPress={()=>this.goBack()}>
+                    <View style={{marginHorizontal:10, marginTop:5}}>
+                        <Text style={{fontSize:15, color:'#004BF6'}}>
+                            取消
+                        </Text>
+                    </View>
+                </TouchableWithoutFeedback>
             </View>);
     }
 
-    renderHeader() {
+    renderCenter() {
+        return (
+            <View style={{flexDirection:'column'}}>
+                <View style={styles.header}>
+                    <Text style={{fontSize:14, color:'gray'}}>
+                        阅读历史
+                    </Text>
+                    <Text style={{fontSize:15, color:'#004BF6'}}>
+                        更多
+                    </Text>
+                </View>
+                <Text style={{fontSize:16, marginLeft:25, marginTop:10, marginBottom:5}}>
+                    王健林唱摇滚受捧，思聪赶快签约
+                </Text>
+                <Text style={{fontSize:16, marginLeft:25}}>
+                    没设置登录并不能看到阅读历史啊
+                </Text>
+            </View>
+        );
+    }
 
+    _onHotkeyPressed(key) {
+        this.setState({
+            searchTxt: key,
+            isResultList: true
+        })
+    }
+
+    renderHotWords() {
+        let hotwords = [];
+        for (let hotword of this.state.hotwords) {
+            let view = (
+                <TouchableWithoutFeedback key={hotword['hotWord']}
+                                          onPress={()=>this._onHotkeyPressed(hotword['hotWord'])}>
+                    <View style={styles.itemStyle}>
+                        <Text style={{fontSize:15}}>
+                            {hotword['hotWord']}
+                        </Text>
+                    </View>
+                </TouchableWithoutFeedback>
+            );
+            hotwords.push(view);
+        }
+        return hotwords;
     }
 
     render() {
+
+        let enterView = (
+            <View>
+                {this.renderCenter()}
+                <View style={{flexDirection:'column'}}>
+                    <View style={[styles.header,{marginBottom:5}]}>
+                        <Text style={{fontSize:14, color:'gray'}}>
+                            近期热点
+                        </Text>
+                    </View>
+                    <View style={styles.hotWordsStyle}>
+                        {this.state.hotwords ? this.renderHotWords() : <LoadingView/>}
+                    </View>
+                </View>
+            </View>
+        );
+
+        let resultView = (
+            <View style={{marginTop:60}}>
+                <LoadingView/>
+            </View>
+        );
+
         return (
             <View style={styles.container}>
                 {this.renderNaivBar()}
+                {this.state.isResultList ? resultView : enterView}
             </View>
         );
     }
@@ -93,7 +166,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#EDEDED',
         flexDirection: 'row',
         justifyContent: 'space-between',
-        alignItems: 'flex-end',
+        alignItems: 'center',
         paddingTop: Platform.OS === 'ios' ? 10 : 0
     },
 
@@ -107,6 +180,34 @@ const styles = StyleSheet.create({
         padding: 4,
         marginTop: Platform.OS === 'ios' ? 10 : 0,
         marginLeft: 10
-    }
+    },
 
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        borderBottomWidth: 0.5,
+        borderColor: 'gray',
+        alignItems: 'flex-end',
+        paddingBottom: 5,
+        marginHorizontal: 15,
+        marginTop: 50
+    },
+
+    hotWordsStyle: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        padding: 10,
+    },
+
+    itemStyle: {
+        flex: 0,
+        borderRadius: 2,
+        borderColor: "#DCDCDC",
+        borderWidth: 0.5,
+        marginHorizontal: 10,
+        marginVertical: 5,
+        padding: 8
+    }
 });
